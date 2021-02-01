d4c7100557ef2d1ea58413ed2a8f7e0e72d610ae
Shaedil <Shaedildider@gmail.com>
2021-02-01 13:05:51 -0500

    feat: check for bths grades only

--- script.user.js
+++ script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         PupilPath Transcript GPA Calculator
-// @version      1.0
+// @version      1.1
 // @description  Calculates your GPA based on PupilPath's transcript
 // @match        https://pupilpath.skedula.com/Grades/Transcript/
 // @grant        none
@@ -9,7 +9,10 @@
 (function() {
     'use strict';
     window.addEventListener('load', function() {
-        action();
+        var button = document.createElement("button");
+        button.innerText = "Calculate GPA";
+        button.addEventListener('click', action);
+        document.getElementsByClassName("content-box-header")[0].appendChild(button);
     }, false);
 })();
 
@@ -23,9 +26,12 @@ function action() {
         //iterate through each course
         for (var courseElement of termElement.rows) {
             //grab the details
-            var weightIndicator = courseElement.cells[4].innerHTML.substring(5, 6);
-            var grade = courseElement.cells[6].innerHTML;
-            var credit = courseElement.cells[7].innerHTML;
+            var DBN = courseElement.cells[1].innerHTML;
+            if (DBN == "13K430") {
+                var weightIndicator = courseElement.cells[4].innerHTML.substring(5, 6);
+                var grade = courseElement.cells[6].innerHTML;
+                var credit = courseElement.cells[7].innerHTML;
+            }
 
             //calculate
 
@@ -40,14 +46,18 @@ function action() {
                 } else {
                     weight = 1;
                 }
-
-
                 //calculating what to accumulate
                 gradeAccumulator += parseFloat(grade * weight * credit);
                 creditAccumulator += parseFloat(credit);
             }
         }
     }
+    var gpa = Math.round(((gradeAccumulator / creditAccumulator) + Number.EPSILON) * 1000) / 1000;
+    var injectedHTML = '<div><span><mark><b>';
+    injectedHTML += 'GPA: ';
+    injectedHTML += gpa;
+    injectedHTML += '</b></mark></span></div>';
+    //document.getElementById('transcript_container').insertAdjacentHTML('afterend', injectedHTML);
+    document.getElementById('TranBy').insertAdjacentHTML('afterend', injectedHTML);
 
-    alert(Math.round(((gradeAccumulator / creditAccumulator) + Number.EPSILON) * 1000) / 1000);
 }
